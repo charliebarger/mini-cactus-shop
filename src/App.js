7b95ac688c425f67b4e3b8c2336d0f206f0aa709
charliebarger <charliebarger04@gmail.com>
2021-08-12 16:47:55 -0600

    route to Shop page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import Header from "./componenets/Header";
 import Homepage from "./componenets/Homepage";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Shop from "./componenets/Shop";
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
         <Switch>
-          <Route path="/shop">
-            <span>ITs da shop</span>
-          </Route>
+          <Route path="/shop" component={Shop} />
           <Route exact path="/" component={Homepage} />
         </Switch>
       </div>
