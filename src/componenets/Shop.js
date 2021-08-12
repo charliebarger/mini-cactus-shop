9957be4e4237648ced89348f5cf40134e6999512
charliebarger <charliebarger04@gmail.com>
2021-08-12 16:31:31 -0600

    add Shop componenet

---
+++ src/componenets/Shop.js
@@ -0,0 +1,11 @@
+import React from react 
+
+const Shop = () => {
+    return(
+        <div className = {shop-page}>
+            <h2>Shop Our Cacti!</h2>
+        </div>
+    )
+}
+
+export default Shop
