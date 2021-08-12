333b7e5356359b3e80fd56cc54e17cf1f481bdc6
charliebarger <charliebarger04@gmail.com>
2021-08-12 16:48:25 -0600

    Add product section

--- src/componenets/Shop.js
+++ src/componenets/Shop.js
@@ -1,11 +1,23 @@
-import React from react 
+import React from "react";
 
 const Shop = () => {
-    return(
-        <div className = {shop-page}>
-            <h2>Shop Our Cacti!</h2>
+  return (
+    <div className={"shop-page"}>
+      <h2>Shop Our Cacti!</h2>
+      <div className="product-wrapper">
+        <div className="product">
+          <div className="image-wrapper">
+            <img
+              alt="cactus"
+              src={process.env.PUBLIC_URL + "/images/cacti/barrel-cactus.jpeg"}
+            />
+          </div>
+          <span>Barrel Cactus</span>
+          <span>$12.99</span>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Shop
+export default Shop;
