393bb78521d51f404d3065f41b177f2ab813d2d8
charliebarger <charliebarger04@gmail.com>
2021-08-13 08:55:24 -0600

    style product components

--- src/componenets/Shop.js
+++ src/componenets/Shop.js
@@ -3,7 +3,7 @@ import React from "react";
 const Shop = () => {
   return (
     <div className={"shop-page"}>
-      <h2>Shop Our Cacti!</h2>
+      <h2>Shop Our Cacti</h2>
       <div className="product-wrapper">
         <div className="product">
           <div className="image-wrapper">
@@ -12,6 +12,7 @@ const Shop = () => {
               src={process.env.PUBLIC_URL + "/images/cacti/barrel-cactus.jpeg"}
             />
           </div>
+          <button className="add-to-cart">Add to Cart</button>
           <span>Barrel Cactus</span>
           <span>$12.99</span>
         </div>
