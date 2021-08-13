98b4333a80bbd382beba4587aa840043d3c2139d
charliebarger <charliebarger04@gmail.com>
2021-08-13 12:17:03 -0600

    fix bug in navbar

--- src/componenets/Shop.js
+++ src/componenets/Shop.js
@@ -3,7 +3,7 @@ import React from "react";
 const Shop = () => {
   return (
     <div className={"shop-page"}>
-      <h2>Shop Our Cacti</h2>
+      <h2>SHOP</h2>
       <div className="product-wrapper">
         <div className="product">
           <div className="image-wrapper">
