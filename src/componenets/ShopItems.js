354699b9dcaa223ee2305dd216b65a0f79adf4da
charliebarger <charliebarger04@gmail.com>
2021-08-15 14:40:27 -0600

    add link to individual item page using match

--- src/componenets/ShopItems.js
+++ src/componenets/ShopItems.js
@@ -1,18 +1,23 @@
 import React from "react";
 import products from "../resources/products";
-
+import { Link, useRouteMatch } from "react-router-dom";
 const ShopItems = () => {
+  const match = useRouteMatch();
   return products.map((product) => (
-    <div className="product">
-      <div className="image-wrapper">
-        <img
-          alt={product.name}
-          src={process.env.PUBLIC_URL + `/images/cacti/${product.skew}.png`}
-        />
+    <Link to={`${match.url}/${product.skew}`}>
+      <div className="product">
+        <div className="image-wrapper">
+          <img
+            alt={product.name}
+            src={process.env.PUBLIC_URL + `/images/cacti/${product.skew}.png`}
+          />
+        </div>
+        <span style={{ textTransform: "capitalize" }} className="product-name">
+          {product.name}
+        </span>
+        <span className="product-price">${product.price}</span>
       </div>
-      <span className="product-name">{product.name}</span>
-      <span className="product-price">${product.price}</span>
-    </div>
+    </Link>
   ));
 };
 
