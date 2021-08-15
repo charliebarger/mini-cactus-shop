9b9f471b3a0cfae66859905e7ff7efe57043a8eb
charliebarger <charliebarger04@gmail.com>
2021-08-15 13:53:30 -0600

    style cart page

--- src/componenets/FullCart.js
+++ src/componenets/FullCart.js
@@ -1,18 +1,44 @@
 import React from "react";
 
 const FullCart = () => {
+  const selectItems = [];
+  for (let i = 1; i < 11; i++) {
+    selectItems.push(i);
+  }
+  console.log(selectItems);
   return (
     <div className={"cart-wrapper"}>
-      <h1 className={"cart-header"}>Your Cart</h1>
+      <h2 className={"cart-page-header"}>Your Cart</h2>
       <div className="cart-grid">
-        <span>Item</span>
-        <span>Unit Price</span>
-        <span>Qty</span>
-        <span>Item Total</span>
-        <span>Item</span>
-        <span>Unit Price</span>
-        <span>Qty</span>
-        <span>Item Total</span>
+        <span className="cart-header">Item</span>
+        <span className="cart-header">Unit Price</span>
+        <span className="cart-header">Qty</span>
+        <span className="cart-header">Item Total</span>
+        <div className="item-section">
+          <div className="cart-image-wrapper">
+            <img
+              alt="Barrel Cactus"
+              src={process.env.PUBLIC_URL + "/images/cacti/barrel-cactus.png"}
+            />
+          </div>
+          <span className="cart-item-name">Barrel Cactus</span>
+        </div>
+        <span>$12.99</span>
+        <div>
+          <select>
+            {selectItems.map((item) => (
+              <option>{item}</option>
+            ))}
+          </select>
+        </div>
+        <span>$12.99</span>
+      </div>
+      <div className="checkout-wrapper">
+        <div className="checkout-total">
+          <span>Total : </span>
+          <span>$12.99</span>
+        </div>
+        <button className="checkout-button">Proceed to Checkout</button>
       </div>
     </div>
   );
