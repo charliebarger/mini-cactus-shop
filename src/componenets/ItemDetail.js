54a5abfa5e8be4d683f78051de7100afa6c2746c
charliebarger <charliebarger04@gmail.com>
2021-08-16 13:46:01 -0600

    style Item detail page

--- src/componenets/ItemDetail.js
+++ src/componenets/ItemDetail.js
@@ -3,6 +3,10 @@ import { useParams, useRouteMatch } from "react-router";
 import { LoremIpsum } from "react-lorem-ipsum";
 import products from "../resources/products";
 const ItemDetail = () => {
+  const selectItems = [];
+  for (let i = 1; i < 11; i++) {
+    selectItems.push(i);
+  }
   const { skew } = useParams();
   function findItem() {
     for (let i = 0; i < products.length; i++) {
@@ -20,16 +24,29 @@ const ItemDetail = () => {
           src={process.env.PUBLIC_URL + `/images/cacti/${item.skew}.png`}
         />
       </div>
-      <h3>{item.name}</h3>
-      <button className="checkout-button">Add to Cart</button>
-      <button className="go-back">Go Back</button>
-      <div id="item-details">
-        <span>Details :</span>
-        <LoremIpsum
-          p={1}
-          avgSentencesPerParagraph={4}
-          avgWordsPerSentence={8}
-        />
+      <div className="item-detail-info">
+        <h3 className="item-detail-title">{item.name}</h3>
+        <div id="item-details">
+          <span>Details : </span>
+          <LoremIpsum
+            p={1}
+            avgSentencesPerParagraph={4}
+            avgWordsPerSentence={8}
+          />
+        </div>
+        <div className="quantity">
+          <label for="item-number-select">Quantity</label>
+          <select id="item-number-select">
+            {selectItems.map((item) => (
+              <option>{item}</option>
+            ))}
+          </select>
+        </div>
+        <button className="checkout-button item-detail-checkout-button">
+          Add to Cart
+        </button>
+
+        <button className="go-back">Go Back</button>
       </div>
     </div>
   );
