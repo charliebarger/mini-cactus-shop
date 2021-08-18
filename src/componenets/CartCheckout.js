9c3ba8e16ed2465d4777d152e0683454d9c78da1
charliebarger <charliebarger04@gmail.com>
2021-08-18 15:00:56 -0600

    fixed formatting bug

--- src/componenets/CartCheckout.js
+++ src/componenets/CartCheckout.js
@@ -4,7 +4,10 @@ const CartCheckout = (props) => {
   const [totalCost, setTotalCost] = useState();
   useEffect(() => {
     if (props.items.length > 0) {
-      let cost = props.items.reduce((sum, { totalCost }) => sum + totalCost, 0);
+      let cost = props.items.reduce(
+        (sum, { totalCost }) => sum + Number(totalCost),
+        0
+      );
       console.log(Number(cost));
       setTotalCost(Number(cost.toFixed(2)));
     }
